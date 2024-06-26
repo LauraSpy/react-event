9e09ff907cbd6ad62b6d6e3f6a2a82fda9ac3f3f
LauraSpy <137756546+LauraSpy@users.noreply.github.com>
2024-06-26 10:51:58 +0200

    modif et suppression

--- react-event/src/app.jsx
+++ react-event/src/app.jsx
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.module.css';
+
+
+// La mairie de de ta ville souhaite développer une application très simple permettant de compter les 
+// participants d'activités diverses (match, festival, vote, etc…).
+
+// La principe de l'application est simple, elle doit comporter :
+
+// un compteur
+// un bouton permettant d'incrémenter le compteur
+// un bouton permettant de décrémenter le compteur (il ne doit pas aller en dessous de zéro)
+// un bouton permettant de remettre le compteur à zéro
+// Ajoute des commentaires afin d'expliquer ton code. Une fois l'application développée, partagé votre 
+// code versionné sur Gitlab.
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
